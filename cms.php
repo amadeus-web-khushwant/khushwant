b47d76d313c55419ca2b574ec164377a2a4bc8a2
Khushwant Komal <komal.khushwant@gmail.com>
2026-01-16 18:59:39 +0530

    kg-slider and siteReplaces

--- cms.php
+++ cms.php
@@ -4,7 +4,37 @@ variables([
 	'link-to-sub-node-home' => true,
 ]);
 
-if (nodeIs(SITEHOME))
-	variable('welcome-message', 'Welcome to My Site<br />
-<a href="%url%colouring-books/animals/001-cow--crow--swan/" class="d-block text-center"><img src="%cdn%colouring/animals--001-cow--crow--swan.png" class="img-fluid img-max-600" /></a>
-');
+$wantsKG = nodeIs(SITEHOME);
+
+if ($wantsKG) {
+	includeThemeManager();
+	CanvasTheme::HeadCssFor($subTheme = 'kindergarten');
+	variable('pseudo-sub-theme', $subTheme);
+	if (nodeIs(SITEHOME)) {
+		variable('siteReplaces', [
+			'slider-intro' => variable('byline'),
+			'slider-heading' => str_replace('With ', 'With <span class="color">', variable('name')) . '</span>',
+			'slider-cta-link' => '#todo', 'slider-cta' => 'Buy On Amazon!!',
+			'slider-youtube-id' => 'P3Huse9K6Xs', 'slider-youtube-cta' => 'Why Our Colouring Books',
+			'slider-mood' => '%cdn%colouring/animals--001-cow--crow--swan.png',
+			'slider-figure' => 'https://canvastemplate.com/demos/kindergarten/images/fish2.png',
+			'slider-message' => '<a href="%url%colouring-books/animals/001-cow--crow--swan/" class="btn btn-primary my-0">
+				Why Paint a Cow, Crow and Swan?</a><br /><span class="bg-warning my-1 p-3 d-inline-block rounded-4">It is: <b>Fun / Informative / Creative</b> :)</span>',
+		]);
+	}
+}
+
+function enrichThemeVars($vars, $what) {
+	$subTheme = variable('pseudo-sub-theme');
+
+	if ($what == 'header' && $subTheme == 'kindergarten') {
+		$vars['optional-slider'] = replaceHtml(getSnippet('kg-slider'));
+	}
+
+	return $vars;
+}
+
+function after_footer_assets() {
+	if (nodeIs(SITEHOME))
+		echo getSnippet('slider-footer');
+}
